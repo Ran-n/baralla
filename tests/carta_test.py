c9c96618ea877413dcf8e5bea80d8d30644772c1
Ran-n <ran-n@tutanota.com>
2021-08-09 01:03:33 +0200

    tests

---
+++ tests/carta_test.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python3
+#-*- encoding: utf-8 -*-
+# ------------------------------------------------------
+#+ Autor:	Ran#
+#+ Creado:	09/08/2021 00:54:34
+#+ Editado:	09/08/2021 00:57:13
+# ------------------------------------------------------
+
+import unittest
+
+from ..src.
+
+class TestCarta(unittest.TestCase):
+    pass
+
+# para que corran os tests
+unittest.main()
+
+# ------------------------------------------------------
