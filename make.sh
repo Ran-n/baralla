4bf2deb8e64b1ee3aa3c4d4603eced903a800ba4
Ran-n <ran-n@tutanota.com>
2021-08-09 19:58:38 +0200

    make make make

--- make.sh
+++ make.sh
@@ -2,12 +2,13 @@
 # -----------------------------------
 #+ Autor:	Ran#
 #+ Creado:	09/08/2021 18:27:28
-#+ Editado:	09/08/2021 18:58:51
+#+ Editado:	09/08/2021 19:55:08
 # -----------------------------------
 
+#pytest --cov=src --cov-report=html
 pytest --cov=src
-#coverage report
 coverage html
+#coverage report
 cp .coverage tests/.coverage
 mkdir -p tests/htmlcov
 mv htmlcov/* tests/htmlcov/.
