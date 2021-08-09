06c5c4dd1f2e1f55293422e746dfb866e0c90357
Ran-n <ran-n@tutanota.com>
2021-08-09 18:55:11 +0200

    make prifeto

--- make.sh
+++ make.sh
@@ -2,13 +2,13 @@
 # -----------------------------------
 #+ Autor:	Ran#
 #+ Creado:	09/08/2021 18:27:28
-#+ Editado:	09/08/2021 18:41:59
+#+ Editado:	09/08/2021 18:54:57
 # -----------------------------------
 
 pytest --cov=src
 #coverage report
 coverage html
 cp .coverage tests/.coverage
-mv htmlcov tests/htmlcov
+mv -n htmlcov/ tests/
 
 # -----------------------------------
