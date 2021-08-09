58d7139b3f15fe4ae0ed27ef6b6a12b3a9fe3b18
Ran-n <ran-n@tutanota.com>
2021-08-09 18:42:07 +0200

    mellorado

--- make.sh
+++ make.sh
@@ -2,11 +2,13 @@
 # -----------------------------------
 #+ Autor:	Ran#
 #+ Creado:	09/08/2021 18:27:28
-#+ Editado:	09/08/2021 18:31:56
+#+ Editado:	09/08/2021 18:41:59
 # -----------------------------------
 
 pytest --cov=src
 #coverage report
 coverage html
+cp .coverage tests/.coverage
+mv htmlcov tests/htmlcov
 
 # -----------------------------------
