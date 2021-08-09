1b91d930eb459655874682650ebcdbd5764c7e0f
Ran-n <ran-n@tutanota.com>
2021-08-09 14:35:46 +0200

    coverage

--- tests/baralla_test.py
+++ tests/baralla_test.py
@@ -3,7 +3,7 @@
 # ------------------------------------------------------
 #+ Autor:	Ran#
 #+ Creado:	09/08/2021 00:55:30
-#+ Editado:	09/08/2021 00:55:30
+#+ Editado:	09/08/2021 14:29:48
 # ------------------------------------------------------
 
 import unittest
@@ -13,7 +13,20 @@ sys.path.append('src')
 from baralla import Baralla
 
 class TestBaralla(unittest.TestCase):
-	pass
+
+    def test_get_nome(self):
+        nome = 'nome'
+        b = Baralla(nome=nome)
+        self.assertEqual(b.get_nome(), nome)
+    
+    def test_get_preset(self):
+        preset = 'poker'
+        b = Baralla(preset=preset)
+        self.assertEqual(b.get_preset(), preset)
+
+    def test_get_cartas(self):
+        b = Baralla()
+        self.assertEqual(b.get_cartas(), [])
 
 # para que corran os tests
 unittest.main()
