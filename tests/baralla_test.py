151264d808cde5fa82943439f7e3f8ba4af50615
Ran-n <ran-n@tutanota.com>
2021-08-09 11:50:11 +0200

    coverage

--- tests/baralla_test.py
+++ tests/baralla_test.py
@@ -5,4 +5,17 @@
 #+ Creado:	09/08/2021 00:55:30
 #+ Editado:	09/08/2021 00:55:30
 # ------------------------------------------------------
+
+import unittest
+
+import sys
+sys.path.append('src')
+from baralla import Baralla
+
+class TestBaralla(unittest.TestCase):
+	pass
+
+# para que corran os tests
+unittest.main()
+
 # ------------------------------------------------------
--- tests/carta_test.py
+++ tests/carta_test.py
@@ -3,12 +3,14 @@
 # ------------------------------------------------------
 #+ Autor:	Ran#
 #+ Creado:	09/08/2021 00:54:34
-#+ Editado:	09/08/2021 00:57:13
+#+ Editado:	09/08/2021 01:44:37
 # ------------------------------------------------------
 
 import unittest
 
-from ..src.
+import sys
+sys.path.append('src')
+from carta import Carta
 
 class TestCarta(unittest.TestCase):
     pass
