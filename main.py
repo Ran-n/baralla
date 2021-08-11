8b44224b0c7b41082ba7f0aa31b0e2638d3b00b6
Ran-n <ran-n@tutanota.com>
2021-08-11 12:23:53 +0200

    commit do main

---
+++ main.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+# --------------------------------------------
+#+ Autor:	Ran#
+#+ Creado:	10/08/2021 21:01:21
+#+ Editado:	10/08/2021 21:32:00
+# --------------------------------------------
+
+from src.xogar import Xogar
+from src.carta_alta import CartaAltaEstratexia
+
+Xogar(CartaAltaEstratexia()).crear_baralla()
+
+
+# --------------------------------------------
